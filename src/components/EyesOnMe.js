d5fa4522f0e1c472b199c66e5044f5c96c723432
Matthew <+>
2023-10-29 16:43:24 +0300

    event handling lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+// Code EyesOnMe Component Here<
+import React, { useState } from "react";
+
+function EyesOnMe() {
+  const [focus, setFocus] = useState(false);
+  function focussing() {
+    console.log("Good!");
+    setFocus(true);
+  }
+
+  function blurring() {
+    setFocus(false);
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <div>
+      <button onFocus={focussing} onBlur={blurring}>
+        {focus ? console.log("Good!") : console.log("Hey! Eyes on me!")}
+        <p>Eyes on me</p>
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
+import React, { useState } from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const [pass, setPass] = useState("");
+
+  function handlePass(e) {
+    console.log("Entering password...");
+    setPass(e.target.value);
+  }
+  return (
+    <div>
+      <input type="password" value={pass} onChange={handlePass} />
+    </div>
+  );
 }
 
 export default Keypad;
